48177cd9e4ce41431c2b51a1160905793c0e3ad7
arunkumar413 <arunkumar413@gmail.com>
2022-10-07 21:58:03 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -4,14 +4,15 @@ import './style.css';
 import petStore from './petstore.json';
 
 export default function App() {
-  console.log(petStore);
   const paths = petStore.paths;
+  console.log(paths);
 
   const items = Object.keys(paths);
 
   let elements = items.map(function (item, index) {
     console.log(item);
-    return <h5> {petStore.paths[item].post.description} </h5>;
+    // console.log(petStore.paths[item].put.description);
+    return <h5> {petStore.paths[item].put.description} </h5>;
   });
 
   return <div>{elements}</div>;
