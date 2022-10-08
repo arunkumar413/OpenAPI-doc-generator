36dda5f572f50c1c7dd9c36a0052c5108e602e9f
arunkumar413 <arunkumar413@gmail.com>
2022-10-08 11:29:44 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './style.css';
 
 import petStore from './petstore.json';
@@ -6,21 +6,34 @@ import petStore from './petstore.json';
 export default function App() {
   const paths = petStore.paths;
   const items = Object.keys(paths);
+  const [selectedItem, setSelectedItem] = useState('');
 
   console.log(Object.keys);
+
+  function handleResourceClick(item) {
+    setSelectedItem(item);
+    console.log(item);
+  }
 
   const elements = items.map(function (item, index) {
     if (paths[item].hasOwnProperty('get')) {
       return (
         <div>
-          <h3> {item} </h3>
-          <p> {paths[item].get.description} </p>
+          <h3 onClick={() => handleResourceClick(item)}> {item} </h3>
+          <p> {paths[item].get.summary} </p>
         </div>
       );
     }
   });
 
-  return <div>{elements}</div>;
+  return (
+    <div className="container">
+      <aside>{elements}</aside>{' '}
+      <main>
+        <h2> Main content here </h2>
+      </main>
+    </div>
+  );
 }
 
 // {petStore.paths['/pet'].post.description}
